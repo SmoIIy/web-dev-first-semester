f6e3e3969dcc6d4f5d39e43b92a6c27a29fc68c8
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2025-09-02 12:00:45 +0200

    Lavet en masse gaming

--- web/app.py
+++ web/app.py
@@ -1,24 +1,36 @@
 from flask import Flask, render_template, request
 
-
-
 from icecream import ic
 ic.configureOutput(prefix=f'----- | ', includeContext=True)
 
+
+import x
+
+
+
 app = Flask(__name__)
 
-
-@app.get("/")
-def view_index():
-    return "Janus"
+# Main page
+# friendly url inspired by twitter.com
+# 127.0.0.1/whatever 
+@app.get("/<username>")
+def view_index(username):
+    db, cursor = x.db()
+    q = "SELECT * FROM users WHERE user_username = %s"
+    cursor.execute(q, (username,))
+    user = cursor.fetchone()
+    ic(user)
+    if user:
+        return render_template("index.html", user = user)
+    return "Ja der findes jo ikk en bruger med det username din FUCKING idiot"
 
 # 127.0.0.1/search?first-name
 # in flask, args is everything after the questionmark
-@app.get("/search")
-def view_search():
-    name = request.args.get("first-name", "")
-    last_name = request.args.get("last-name", "")
-    year = request.args.get("year", "")
-    fav_color = request.args.get("fav-color", "")
-    return f"Hi {name} {last_name}, the year is {year}. My favorite color is {fav_color}"
+# @app.get("/search")
+# def view_search():
+#     name = request.args.get("first-name", "")
+#     last_name = request.args.get("last-name", "")
+#     year = request.args.get("year", "")
+#     fav_color = request.args.get("fav-color", "")
+#     return f"Hi {name} {last_name}, the year is {year}. My favorite color is {fav_color}"
 
--- web/x.py
+++ web/x.py
@@ -7,13 +7,11 @@ ic.configureOutput(prefix=f'***** | ', includeContext=True)
 
 ##############################
 def db():
-    host = "mysql"
-    database = ""
     db = mysql.connector.connect(
-        host = host,      # Replace with your MySQL server's address or docker service name "mysql"
-        user = "",  # Replace with your MySQL username
-        password = "",  # Replace with your MySQL password
-        database = database   # Replace with your MySQL database name
+        host = "mysql",      # Replace with your MySQL server's address or docker service name "mysql"
+        user = "root",  # Replace with your MySQL username
+        password = "password",  # Replace with your MySQL password
+        database = "company"   # Replace with your MySQL database name
     )
     cursor = db.cursor(dictionary=True)
     return db, cursor
