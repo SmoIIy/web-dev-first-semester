e07cac8483f3d91627257e135ec7deb61b1b112c
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2025-09-06 21:44:22 +0200

    Calculator

---
+++ ux/exercises_week_2/script.js
@@ -0,0 +1,20 @@
+document.querySelector(".tax-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const inputAmount = document.querySelector("#amount");
+    const inputPercentage = document.querySelector("#percentage");
+
+    const taxAmountOutput = document.querySelector("#tax-amount");
+    const finalAmountOuput = document.querySelector("#final-amount");
+
+    const calculateTaxPercentage = 100 - parseFloat(inputPercentage.value);
+    //mængden betalt i tax
+    const taxAmountValue =
+        (parseFloat(inputAmount.value) * parseFloat(inputPercentage.value)) /
+        100;
+    //Mængden der er tilbage efter tax
+    const finalAmountValue =
+        parseFloat(inputAmount.value) * (calculateTaxPercentage / 100);
+
+    taxAmountOutput.innerHTML = taxAmountValue;
+    finalAmountOuput.innerHTML = finalAmountValue;
+});
