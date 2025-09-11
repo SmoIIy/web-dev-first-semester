e4cb972d2513025d9d3b9854539cbed66858a13e
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2025-09-11 08:57:40 +0200

    innerHTML -> innerText

--- ux/exercises_week_2/script.js
+++ ux/exercises_week_2/script.js
@@ -1,20 +1,20 @@
 document.querySelector(".tax-form").addEventListener("submit", function (e) {
-	e.preventDefault();
-	const inputAmount = document.querySelector("#amount");
-	const inputPercentage = document.querySelector("#percentage");
+    e.preventDefault();
+    const inputAmount = document.querySelector("#amount");
+    const inputPercentage = document.querySelector("#percentage");
 
-	const taxAmountOutput = document.querySelector("#tax-amount");
-	const finalAmountOuput = document.querySelector("#final-amount");
+    const taxAmountOutput = document.querySelector("#tax-amount");
+    const finalAmountOuput = document.querySelector("#final-amount");
 
-	const calculateTaxPercentage = 100 - parseFloat(inputPercentage.value);
-	//mængden betalt i tax
-	const taxAmountValue =
-		(parseFloat(inputAmount.value) * parseFloat(inputPercentage.value)) /
-		100;
-	//Mængden der er tilbage efter tax
-	const finalAmountValue =
-		parseFloat(inputAmount.value) * (calculateTaxPercentage / 100);
+    const calculateTaxPercentage = 100 - parseFloat(inputPercentage.value);
+    //mængden betalt i tax
+    const taxAmountValue =
+        (parseFloat(inputAmount.value) * parseFloat(inputPercentage.value)) /
+        100;
+    //Mængden der er tilbage efter tax
+    const finalAmountValue =
+        parseFloat(inputAmount.value) * (calculateTaxPercentage / 100);
 
-	taxAmountOutput.innerHTML = taxAmountValue;
-	finalAmountOuput.innerHTML = finalAmountValue;
+    taxAmountOutput.innerText = taxAmountValue.toFixed(2);
+    finalAmountOuput.innerText = finalAmountValue.toFixed(2);
 });
--- ux/exercises_week_2/temp.js
+++ ux/exercises_week_2/temp.js
@@ -1,52 +1,52 @@
 document.querySelector(".temp-form").addEventListener("submit", function (e) {
-	e.preventDefault();
-	const initialValueOutput = document.querySelector("#initial-value");
-	const convertedValueOutput = document.querySelector("#converted-value");
-	const from = document.querySelector("#from").value;
-	const to = document.querySelector("#to").value;
-	const input = parseFloat(document.querySelector("#convert-input").value);
-	let output;
-	let initialLetter;
-	let convertedLetter;
-	if (from === "celcius") {
-		initialLetter = "C";
-		if (to === "celcius") {
-			convertedLetter = "C";
-			output = input;
-		} else if (to === "fahrenheit") {
-			convertedLetter = "F";
-			output = input * (9 / 5) + 32;
-		} else if (to === "kelvin") {
-			convertedLetter = "K";
-			output = input + 273.15;
-		}
-	}
-	if (from === "fahrenheit") {
-		initialLetter = "F";
-		if (to === "fahrenheit") {
-			convertedLetter = "F";
-			output = input;
-		} else if (to === "celcius") {
-			convertedLetter = "C";
-			output = ((input - 32) * 5) / 9;
-		} else if (to === "kelvin") {
-			convertedLetter = "K";
-			output = ((input - 32) * 5) / 9 + 273.15;
-		}
-	}
-	if (from === "kelvin") {
-		initialLetter = "K";
-		if (to === "kelvin") {
-			convertedLetter = "K";
-			output = input;
-		} else if (to === "fahrenheit") {
-			convertedLetter = "F";
-			output = ((input - 273.15) * 9) / 5 + 32;
-		} else if (to === "celcius") {
-			convertedLetter = "C";
-			output = input - 273.15;
-		}
-	}
-	initialValueOutput.innerHTML = input.toFixed(2) + initialLetter;
-	convertedValueOutput.innerHTML = output.toFixed(2) + convertedLetter;
+    e.preventDefault();
+    const initialValueOutput = document.querySelector("#initial-value");
+    const convertedValueOutput = document.querySelector("#converted-value");
+    const from = document.querySelector("#from").value;
+    const to = document.querySelector("#to").value;
+    const input = parseFloat(document.querySelector("#convert-input").value);
+    let output;
+    let initialLetter;
+    let convertedLetter;
+    if (from === "celcius") {
+        initialLetter = "C";
+        if (to === "celcius") {
+            convertedLetter = "C";
+            output = input;
+        } else if (to === "fahrenheit") {
+            convertedLetter = "F";
+            output = input * (9 / 5) + 32;
+        } else if (to === "kelvin") {
+            convertedLetter = "K";
+            output = input + 273.15;
+        }
+    }
+    if (from === "fahrenheit") {
+        initialLetter = "F";
+        if (to === "fahrenheit") {
+            convertedLetter = "F";
+            output = input;
+        } else if (to === "celcius") {
+            convertedLetter = "C";
+            output = ((input - 32) * 5) / 9;
+        } else if (to === "kelvin") {
+            convertedLetter = "K";
+            output = ((input - 32) * 5) / 9 + 273.15;
+        }
+    }
+    if (from === "kelvin") {
+        initialLetter = "K";
+        if (to === "kelvin") {
+            convertedLetter = "K";
+            output = input;
+        } else if (to === "fahrenheit") {
+            convertedLetter = "F";
+            output = ((input - 273.15) * 9) / 5 + 32;
+        } else if (to === "celcius") {
+            convertedLetter = "C";
+            output = input - 273.15;
+        }
+    }
+    initialValueOutput.innerText = input.toFixed(2) + initialLetter;
+    convertedValueOutput.innerText = output.toFixed(2) + convertedLetter;
 });
